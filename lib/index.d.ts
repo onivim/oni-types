e513e84a5871bb27caaea28a1084f6d5b60d49b1
Bryan Phelps <bryphe@outlook.com>
2018-05-15 09:30:33 -0700

    Bump version, rebuild

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs";
 import "rxjs/add/operator/auditTime";
 import "rxjs/add/operator/combineLatest";
 import "rxjs/add/operator/debounceTime";
